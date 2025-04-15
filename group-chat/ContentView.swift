3f795835e77a2b7b6140e53fd8612b55dd18ce5c
Viktor Ban <viktorrichardban@gmail.com>
2025-04-15 17:29:41 +0200

    Initial Commit

---
+++ group-chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  group-chat
+//
+//  Created by Viktor Bán on 2025. 04. 15..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ group-chat/group_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  group_chatApp.swift
+//  group-chat
+//
+//  Created by Viktor Bán on 2025. 04. 15..
+//
+
+import SwiftUI
+
+@main
+struct group_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
